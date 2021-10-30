ee7b86d48e6152245706a8eab5d0fdf1e565852f
presium575730 <presium575730@gmail.com>
2021-10-30 20:15:08 +0900

    start

--- feed_cat_project/feed_cat_project/main.c
+++ feed_cat_project/feed_cat_project/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <windows.h>
 
+
+// 함수 선언
+void start();
+
+
+
+
+// 메인 함수
 int main() {
 
+	system("mode con cols=100 lines=30");
+
 	return 0;
+}
+
+
+void start() {
+
+
+
 }
