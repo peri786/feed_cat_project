42803ba8e8642652b7363728dd3ca4ae66faa8cd
presium575730 <presium575730@gmail.com>
2021-10-31 00:53:09 +0900

    title

--- feed_cat_project/feed_cat_project/main.c
+++ feed_cat_project/feed_cat_project/main.c
@@ -1,24 +1,44 @@
 #include <stdio.h>
 #include <windows.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define BOARD_X 30 // 가로
+#define BOARD_Y 20 // 세로
+
+
+char board[BOARD_X][BOARD_Y];
 
 
 // 함수 선언
-void start();
+void title();
+void initiolize();
 
 
 
+
+void title() {
+
+	printf("\n\n\n\n\n");
+	printf(" _______  _______  _______  _______       ______      ___     .___________.    __   __    \n");
+	printf("|   ____||   ____||   ____||      \\      /      |    /   \\    |           |   |  | |  | \n");
+	printf("|  |__   |  |__   |  |__   |  .--.  |   |  ,----'   /  ^  \\   `---|  |----`   |  | |  |  \n");
+	printf("|   __|  |   __|  |   __|  |  |  |  |   |  |       /  /_\\  \\      |  |        |  | |  | \n");
+	printf("|  |     |  |____ |  |____ |  '--'  |   |  `----. /  _____  \\     |  |        |__| |__|  \n");
+	printf("|__|     |_______||_______||_______/    \\______| /__/     \\__\\    |__|        (__) (__)\n");
+}
+
+void initiolize() {
+
+
+}
 
 // 메인 함수
 int main() {
 
-	system("mode con cols=100 lines=30");
+	system("mode con cols=110 lines=35");
+
+	title();
 
 	return 0;
-}
-
-
-void start() {
-
-
-
 }
